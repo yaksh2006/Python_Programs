3d2d6ce0a143ba397e1439817381f3dfa951d170
yaksh2006 <yk491232@gmail.com>
2026-03-07 11:22:00 +0530

    Initial commit

--- ANN_ML_practical1/basics/p1.py
+++ ANN_ML_practical1/basics/p1.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-nums = np.array([[1, 3,4 , 4,8 , 67, 7, 88, 96, 10]])
+nums = np.array([[1, 3,4 , 4,8 , 67, 70, 88, 96, 10]])
 
 print(nums.shape, "\n")
 print(nums.size, "\n")
